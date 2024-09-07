1f018f6755586ae251922643df8f57a0716855f1
Denu8 <kingdenuwan07@gmail.com>
2024-09-08 01:19:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_406.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "||```HEY DEAR I AM ALIVE NOW...💀❗``` || \n\n`🪄🛠️OUR WHATSAPP CHANEL ;`\n\n_https://whatsapp.com/channel/0029VaePgYXBqbr6Vkg75n1j 🤖🇱🇰_\n\n`|| *🪄🛠️OUR YOUTUBE CHANEL ;`\n\n_http://www.youtube.com/@Denuwa_Editz90 🤖🇱🇰_ \n\n`||🪄🛠️BOT OWNER NUMBER ;`\n\n+94722336454 🤖🇱🇰\n\n || `°°Powered By BOXTER MD°°`",
+ALIVE_MSG: process.env.ALIVE_MSG || "`Hey ${pushname} I'm Online Now...😚💚 || > https://whatsapp.com/channel/0029VaePgYXBqbr6Vkg75n1j 💫",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
