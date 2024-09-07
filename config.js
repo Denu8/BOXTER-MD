cb066811f777bfce070058f9be76110b3c424f12
Denu8 <kingdenuwan07@gmail.com>
2024-09-07 21:32:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_406.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello ${pushname} I'm Online Now...😎💚 | Get ( .menu ) For Commands 👀📜*\n\n> Join Our Support Chanel = https://whatsapp.com/channel/0029VaePgYXBqbr6Vkg75n1j 🙌
+ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello ${pushname} I'm Online Now...😎💚 | Get ( .menu ) For Commands 👀📜*\n\n> Join Our Support Chanel = https://whatsapp.com/channel/0029VaePgYXBqbr6Vkg75n1j 🙌,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
