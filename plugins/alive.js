2094f37b77b32eccba92d06d62e84aaccdb36457
Denu8 <kingdenuwan07@gmail.com>
2024-10-09 15:38:36 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,20 +1,78 @@
-const {readEnv} = require('../lib/database')
-const {cmd , commands} = require('../command')
-
+const config = require('../config')
+const os = require('os')
+const {
+    cmd,
+    commands
+} = require('../command')
+const {
+    getBuffer,
+    getGroupAdmins,
+    getRandom,
+    h2k,
+    isUrl,
+    Json,
+    runtime,
+    sleep,
+    fetchJson
+} = require('../lib/functions')
 cmd({
-    pattern: "alive",
-    desc: "Check bot online or no.",
-    category: "main",
-    react: "👋",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-const config = await readEnv();
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+        pattern: "alive",
+        react: "🍬",
+        alias: ["online", "test", "bot"],
+        desc: "Check bot online or no.",
+        category: "other",
+        use: '.alive',
+        filename: __filename
+    },
+    async (conn, mek, m, {
+        from,
+        prefix,
+        pushname,
+        reply
+    }) => {
+        try {
+            if (os.hostname().length == 12) hostname = 'replit'
+            else if (os.hostname().length == 36) hostname = 'heroku'
+            else if (os.hostname().length == 8) hostname = 'koyeb'
+            else hostname = os.hostname()
+            let monspace = '```'
+            const sssf = `${monspace}👋 Hello ${pushname} I'm alive now${monspace}
 
+*👾 Im boxter-MD whatsapp bot*
+    
+> *Version:* ${require("../package.json").version}
+> *Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+> *Runtime:* ${runtime(process.uptime())}
+> *Platform:* ${hostname}
+    
+*🍭 Have A Nice Day 🍭*`
+
+            let buttons = [{
+                    name: "cta_url",
+                    buttonParamsJson: JSON.stringify({
+                        display_text: config.BTN,
+                        url: config.BTNURL,
+                        merchant_url: config.BTNURL
+                    }),
+                },
+                {
+                    name: "quick_reply",
+                    buttonParamsJson: JSON.stringify({
+                        display_text: "Get Menu",
+                        id: ".menu"
+                    }),
+                }
+            ]
+            let opts = {
+                image: config.LOGO,
+                header: '',
+                footer: config.FOOTER,
+                body: sssf
+
+            }
+            return await conn.sendButtonMessage(from, buttons, m, opts)
+        } catch (e) {
+            reply('*Error !!*')
+            console.log(e)
+        }
+    })
